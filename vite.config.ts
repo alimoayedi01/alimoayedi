62592c9b6f10827072766dc0f55a41c3bd4e8ddd
alimoayedi01 <alimoayedi79@gmail.com>
2025-10-19 20:46:29 +0330

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// Replace "alimoayedi" with your repository name
+const repoName = "alimoayedi";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? `/${repoName}/` : "/", // <-- Add this line
   server: {
     host: "::",
     port: 8080,
